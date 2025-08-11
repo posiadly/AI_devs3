ec75fd8a283f506db9d9c810f866ac7f09feb04d
Piotr Osiadly <piotr.osiadly@proaxia-consulting.com>
2025-08-11 23:24:12 +0200

    Secret03. Test before first deploy into azyl.ag3nts.org

---
+++ src/Secret03/app.ts
@@ -0,0 +1,13 @@
+//create express server
+import express from "express";
+
+const app = express();
+const port = 55962; //process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
